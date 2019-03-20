06be96cb29b5d9a145cb5b0789016d7093273c6c
Andrii Paias <Andrii_Paias@epam.com>
2019-03-20 15:32:24 +0200

    Add method for getting providers by list of ids
    Jira: MODKBEKBJ-187
    Add unimplemented method to interface

--- src/main/java/org/folio/holdingsiq/service/ProviderHoldingsIQService.java
+++ src/main/java/org/folio/holdingsiq/service/ProviderHoldingsIQService.java
@@ -1,5 +1,6 @@
 package org.folio.holdingsiq.service;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.folio.holdingsiq.model.Sort;
@@ -12,6 +13,7 @@ public interface ProviderHoldingsIQService {
   CompletableFuture<Long> getVendorId();
   CompletableFuture<VendorById> retrieveProvider(long id);
   CompletableFuture<Vendors> retrieveProviders(String q, int page, int count, Sort sort);
+  CompletableFuture<Vendors> retrieveProviders(List<Long> ids);
   CompletableFuture<VendorById> updateProvider(long id, VendorPut rmapiVendor);
 
 }
