d5860cb0a074334051085fa7ce2c64fd85819f2e
Andrii Paias <Andrii_Paias@epam.com>
2019-12-10 16:00:27 +0200

    Remove duplication of resources fields
    Jira: MODKBEKBJ-335

--- src/main/java/org/folio/holdingsiq/model/CustomerResources.java
+++ src/main/java/org/folio/holdingsiq/model/CustomerResources.java
@@ -2,15 +2,22 @@ package org.folio.holdingsiq.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Value;
 
-@Value
-@Builder(toBuilder = true)
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CustomerResources {
+@JsonAutoDetect(creatorVisibility = JsonAutoDetect.Visibility.ANY)
+public class CustomerResources extends ResourceBase {
 
   @JsonProperty("titleId")
   private Integer titleId;
@@ -33,9 +40,6 @@ public class CustomerResources {
   @JsonProperty("locationId")
   private Integer locationId;
 
-  @JsonProperty("isSelected")
-  private Boolean isSelected;
-
   @JsonProperty("isTokenNeeded")
   private Boolean isTokenNeeded;
 
@@ -45,40 +49,62 @@ public class CustomerResources {
   @JsonProperty("visibilityData")
   private VisibilityInfo visibilityData;
 
-  @JsonProperty("proxy")
-  private Proxy proxy;
-
   @JsonProperty("managedCoverageList")
   private List<CoverageDates> managedCoverageList;
-
-  @JsonProperty("customCoverageList")
-  private List<CoverageDates> customCoverageList;
-
-  @JsonProperty("coverageStatement")
-  private String coverageStatement;
 
   @JsonProperty("managedEmbargoPeriod")
   private EmbargoPeriod managedEmbargoPeriod;
 
-  @JsonProperty("customEmbargoPeriod")
-  private EmbargoPeriod customEmbargoPeriod;
-
-  @JsonProperty("url")
-  private String url;
-
-  @JsonProperty("userDefinedField1")
-  private String userDefinedField1;
-
-  @JsonProperty("userDefinedField2")
-  private String userDefinedField2;
-
-  @JsonProperty("userDefinedField3")
-  private String userDefinedField3;
-
-  @JsonProperty("userDefinedField4")
-  private String userDefinedField4;
-
-  @JsonProperty("userDefinedField5")
-  private String userDefinedField5;
-
+  @JsonCreator
+  @Builder(builderMethodName = "customerResourcesBuilder")
+  CustomerResources(
+    @JsonProperty("coverageStatement") String coverageStatement,
+    @JsonProperty("isSelected") Boolean isSelected,
+    @JsonProperty("customEmbargoPeriod") EmbargoPeriod customEmbargoPeriod,
+    @JsonProperty("proxy") Proxy proxy,
+    @JsonProperty("url") String url,
+    @JsonProperty("customCoverageList") List<CoverageDates> customCoverageList,
+    @JsonProperty("userDefinedField1") String userDefinedField1,
+    @JsonProperty("userDefinedField2") String userDefinedField2,
+    @JsonProperty("userDefinedField3") String userDefinedField3,
+    @JsonProperty("userDefinedField4") String userDefinedField4,
+    @JsonProperty("userDefinedField5") String userDefinedField5,
+    @JsonProperty("titleId") Integer titleId,
+    @JsonProperty("packageId") Integer packageId,
+    @JsonProperty("packageName") String packageName,
+    @JsonProperty("isPackageCustom") Boolean isPackageCustom,
+    @JsonProperty("vendorId") Integer vendorId,
+    @JsonProperty("vendorName") String vendorName,
+    @JsonProperty("locationId") Integer locationId,
+    @JsonProperty("isTokenNeeded") Boolean isTokenNeeded,
+    @JsonProperty("packageType") String packageType,
+    @JsonProperty("visibilityData") VisibilityInfo visibilityData,
+    @JsonProperty("managedCoverageList") List<CoverageDates> managedCoverageList,
+    @JsonProperty("managedEmbargoPeriod") EmbargoPeriod managedEmbargoPeriod
+  ) {
+    super(
+      coverageStatement,
+      isSelected,
+      customEmbargoPeriod,
+      proxy,
+      url,
+      customCoverageList,
+      userDefinedField1,
+      userDefinedField2,
+      userDefinedField3,
+      userDefinedField4,
+      userDefinedField5);
+    this.titleId =  titleId;
+    this.packageId =  packageId;
+    this.packageName =  packageName;
+    this.isPackageCustom =  isPackageCustom;
+    this.vendorId =  vendorId;
+    this.vendorName =  vendorName;
+    this.locationId =  locationId;
+    this.isTokenNeeded =  isTokenNeeded;
+    this.packageType =  packageType;
+    this.visibilityData =  visibilityData;
+    this.managedCoverageList =  managedCoverageList;
+    this.managedEmbargoPeriod =  managedEmbargoPeriod;
+  }
 }
