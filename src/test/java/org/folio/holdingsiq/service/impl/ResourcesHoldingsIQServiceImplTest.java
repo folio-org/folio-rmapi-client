38ba29f5d7fae3fa6cd01eb44be423a1f43dcfd5
shorasul96 <shorasul_shoazimov@epam.com>
2023-04-19 05:28:07 +0500

    FHIQC-31: some tests

--- src/test/java/org/folio/holdingsiq/service/impl/ResourcesHoldingsIQServiceImplTest.java
+++ src/test/java/org/folio/holdingsiq/service/impl/ResourcesHoldingsIQServiceImplTest.java
@@ -4,6 +4,8 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static org.folio.holdingsiq.service.util.DataUtils.getResourceBody;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
@@ -11,15 +13,17 @@ import com.github.tomakehurst.wiremock.http.RequestMethod;
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder;
 import com.github.tomakehurst.wiremock.matching.UrlPattern;
 import io.vertx.core.Vertx;
+import lombok.SneakyThrows;
 import org.apache.http.HttpStatus;
+import org.folio.holdingsiq.model.ResourceSelectedPayload;
+import org.folio.holdingsiq.service.ResourcesHoldingsIQService;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.folio.holdingsiq.model.ResourceSelectedPayload;
-import org.folio.holdingsiq.service.ResourcesHoldingsIQService;
-
 public class ResourcesHoldingsIQServiceImplTest extends HoldingsIQServiceTestConfig {
 
+  private final static String URL = "/rm/rmaccounts/" + STUB_CUSTOMER_ID + "/vendors/" +
+    VENDOR_ID + "/packages/" + PACKAGE_ID + "/titles/" + TITLE_ID;
   private ResourcesHoldingsIQService service;
 
 
@@ -32,9 +36,7 @@ public class ResourcesHoldingsIQServiceImplTest extends HoldingsIQServiceTestCon
   public void testPostResources() {
     ResourceSelectedPayload resourceSelectedPayload = new ResourceSelectedPayload(false, "titleName",
       "pubType", STUB_BASE_URL);
-    var urlPattern = new UrlPattern(equalTo(
-      "/rm/rmaccounts/" + STUB_CUSTOMER_ID + "/vendors/" + VENDOR_ID + "/packages/" + PACKAGE_ID + "/titles/" + TITLE_ID),
-      false);
+    var urlPattern = new UrlPattern(equalTo(URL), false);
     wiremockServer.stubFor(
       put(urlPattern).willReturn(aResponse().withStatus(HttpStatus.SC_NO_CONTENT))
     );
@@ -49,10 +51,23 @@ public class ResourcesHoldingsIQServiceImplTest extends HoldingsIQServiceTestCon
   }
 
   @Test
+  @SneakyThrows
+  public void testRetrieveResources() {
+    var urlPattern = new UrlPattern(equalTo(URL), false);
+
+    wiremockServer.stubFor(get(urlPattern)
+      .willReturn(aResponse().withStatus(HttpStatus.SC_OK)
+        .withBody(getResourceBody())));
+
+    var completableFuture = service.retrieveResource(resourceId);
+    assertTrue(isCompletedNormally(completableFuture));
+    assertNotNull(completableFuture.get().getCustomerResourcesList().get(0).getProxy().getProxiedUrl());
+    WireMock.verify(new RequestPatternBuilder(RequestMethod.GET, urlPattern));
+  }
+
+  @Test
   public void testUpdateResources() {
-    var urlPattern = new UrlPattern(equalTo(
-      "/rm/rmaccounts/" + STUB_CUSTOMER_ID + "/vendors/" + VENDOR_ID + "/packages/" + PACKAGE_ID + "/titles/" + TITLE_ID),
-      false);
+    var urlPattern = new UrlPattern(equalTo(URL), false);
     wiremockServer.stubFor(
       put(urlPattern).willReturn(aResponse().withStatus(HttpStatus.SC_NO_CONTENT))
     );
@@ -64,9 +79,7 @@ public class ResourcesHoldingsIQServiceImplTest extends HoldingsIQServiceTestCon
 
   @Test
   public void testDeleteResource() {
-    var urlPattern = new UrlPattern(equalTo(
-      "/rm/rmaccounts/" + STUB_CUSTOMER_ID + "/vendors/" + VENDOR_ID + "/packages/" + PACKAGE_ID + "/titles/" + TITLE_ID),
-      false);
+    var urlPattern = new UrlPattern(equalTo(URL), false);
     wiremockServer.stubFor(
       put(urlPattern).willReturn(aResponse().withStatus(HttpStatus.SC_NO_CONTENT))
     );
