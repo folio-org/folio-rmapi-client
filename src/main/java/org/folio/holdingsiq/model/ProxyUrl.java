82417ab0574e1d8b2cd7d24228fb7d1815b95b57
shorasul96 <shorasul_shoazimov@epam.com>
2023-04-25 15:57:22 +0500

    ...

--- src/main/java/org/folio/holdingsiq/model/ProxyUrl.java
+++ src/main/java/org/folio/holdingsiq/model/ProxyUrl.java
@@ -2,13 +2,7 @@ package org.folio.holdingsiq.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
