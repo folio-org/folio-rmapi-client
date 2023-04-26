721d6b07d8f8085c325eab240051884a584281b3
Shorasul Shoazimov <42607045+shorasul96@users.noreply.github.com>
2023-04-26 16:52:08 +0500

    FHIQC-31: Fixed implementation builder on proxy class (#78)

--- src/main/java/org/folio/holdingsiq/model/ProxyUrl.java
+++ src/main/java/org/folio/holdingsiq/model/ProxyUrl.java
@@ -2,18 +2,22 @@ package org.folio.holdingsiq.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-import lombok.extern.jackson.Jacksonized;
 
-@Value
-@Jacksonized
+@Getter
+@ToString
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ProxyUrl extends Proxy {
 
   @JsonProperty("proxiedUrl")
-  String proxiedUrl;
+  private String proxiedUrl;
 }
